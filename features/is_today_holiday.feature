70bb6b036331a251141e0face2bb75c3c08d4396
AvinashAmbati <avinash.ambati@adroitent.com>
2018-06-01 13:22:29 +0530

    holiday helper

---
+++ features/is_today_holiday.feature
@@ -0,0 +1,15 @@
+
+Feature: Is today holiday?
+  Everybody wants to know whether today is holiday
+
+  Scenario: Saturday is holiday
+    Given today is Saturday
+    When I ask whether it's holiday
+    Then I should be told "Yes!It's Saturday and Holiday" 
+
+
+
+    Scenario: Thursday isn't holiday
+      Given today is Thursday
+      When I ask whether it's holiday
+      Then I should be told "Nope"
--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -12,17 +12,22 @@
 
 
  module FridayStepHelper
-   def is_it_friday?(day)
-	 # 'Nope'
-	 if day == "Friday"
-	  return "Yay"
-	 elsif day =="Sunday"
-	 return "Nope"
-	 else
-	 return "Neither Friday Nor Sunday "
-	 end 	
-   end
+ 	#Method for friday finder
+	def is_it_friday?(day)
+		# 'Nope'
+		if day == "Friday"
+	 		return "Yay"
+		elsif day =="Sunday"
+			return "Nope"
+		else
+			return "Neither Friday Nor Sunday "
+		end 	
+	end
+
  end
+
+
+
  World FridayStepHelper
 
  Given("today is Sunday") do
@@ -33,25 +38,46 @@
    @today = 'Friday'
  end
 
-
  When("I ask whether it's Friday yet") do
    @actual_answer = is_it_friday?(@today)
  end
 
  Then("I should be told {string}") do |expected_answer|
-   expect(@actual_answer).to eq(expected_answer)
+   expect(@actual_answer ).to eq(expected_answer)
  end
 
 
 
- # When("I ask whether it's Sunday yet") do
- #   @actual_answer = is_it_friday?(@today)
- # end
+ module HolidayHelper
+   		#Method for holiday helper
+	def is_today_holiday?(day)
+		# 'Nope'
+		if day == "Thursday"
+			return "Nope"
+		elsif day =="Saturday"
+			return "Yes!It's Saturday and Holiday"
+		else
+			return "Neither Thursday Nor Saturday "
+		end
+	end
 
- # Then("I should be told {string}") do |expected_answer|
- #   expect(@actual_answer).to eq(expected_answer)
- # end
+ end
 
-# def is_it_friday?(day)
-#   'Nope'
-# end
+ 
+
+ World HolidayHelper 
+
+ Given("today is Saturday") do
+   @today = 'Saturday'
+ end
+
+ Given("today is Thursday") do
+   @today = 'Thursday'
+ end
+ When("I ask whether it's holiday") do
+   @actual_answer = is_today_holiday?(@today)
+ end
+
+ Then("I should be told1 {string}") do |expected_answer|
+   expect(@actual_answer).to eq(expected_answer)
+ end
